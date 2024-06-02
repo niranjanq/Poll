22b00d903ef2455e3e3c8bd3ae0a157a4382aa42
Niranjan_Panda <rockzmantu200@gmail.com>
2024-06-02 21:02:03 +0530

    fix: resolving errors

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,16 +1,17 @@
 <footer>
-<div class="copyright">Copyright @2024 Niranjan</div>
+  <div class="copyright">Copyright @Niranjan</div>
 </footer>
+
 <style>
-    footer{
-        padding: 40px;
-        text-align: center;
-    }
-    .copyright{
-        color:#aaa;
-        font-size: 14px;
-        display:inline-block;
-        padding:20px;
-        border-top: 1px solid #ddd;
-    }
+  footer {
+    padding: 40px;
+    text-align: center;
+  }
+  .copyright {
+    color: #aaa;
+    font-size: 14px;
+    display: inline-block;
+    padding: 20px;
+    border-top: 1px solid #ddd;
+  }
 </style>
